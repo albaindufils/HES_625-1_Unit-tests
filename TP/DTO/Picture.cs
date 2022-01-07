ff2b8e9a46eb5d7a4c0ec3223a3f92af72eae510
draguarMatth <50259605+draguarMatth@users.noreply.github.com>
2022-01-07 08:23:32 +0100

    Ajout DTO Picture pour sauvegarde Image

---
+++ TP/DTO/Picture.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace DTO
+{
+    public class Picture
+    {
+
+        public string Name { get; set; }
+
+        public string Path { get; set; }
+
+        public List<string> AppliedFilter { get; set; }
+
+        public byte[] ImageByteArray (Bitmap bmp)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                ImageBitmap.Save(memoryStream, ImageFormat.Bmp);
+                ImageByteArray = memoryStream.ToArray();
+                return ImageByteArray;
+            }
+        }
+
+    }
+}
